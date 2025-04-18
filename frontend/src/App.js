4230fd11065ae37af85e2632081be7362e18c37f
Rohith2601 <2023mcb1312@iitrpr.ac.in>
2025-04-18 17:28:45 +0530

    before oreo break

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm'; // Ensure the correct import path
 import LoginForm from './components/Login'; // Ensure the correct import path
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
