bd3665bbb46c3319fe9fd4bbaf98e4f6a45c68df
prathvik1246 <2023mcb1307@iitrpr.ac.in>
2025-04-18 15:46:53 +0530

    Login Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm'; // Ensure the correct import path
 import LoginForm from './components/Login'; // Ensure the correct import path
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
